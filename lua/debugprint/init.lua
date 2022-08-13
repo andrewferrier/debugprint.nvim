43b9c79f727818b34d83fb27a6808d634c551f99
Andrew Ferrier <andrewferrier@example.com>
2022-08-13 22:38:22 +0100

    refactor: Move funcs to utils.lua

--- lua/debugprint/init.lua
+++ lua/debugprint/init.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local utils = require('debugprint.utils')
+
 local opts
 
 OPTION_DEFAULTS = {
@@ -60,92 +62,6 @@ local indent_line = function(current_line)
     end
 end
 
-local find_treesitter_variable = function()
-    local function requiref(module)
-        require(module)
-    end
-
-    local ts_utils_test = pcall(requiref, "nvim-treesitter.ts_utils")
-
-    if not ts_utils_test then
-        return nil
-    else
-        local ts_utils = require("nvim-treesitter.ts_utils")
-        -- Once get_node_at_cursor() is in NeoVim core, the nvim-treesitter
-        -- dependency can be removed: https://github.com/neovim/neovim/pull/18232
-        local node = ts_utils.get_node_at_cursor()
-
-        if node == nil then
-            return nil
-        else
-            local node_type = node:type()
-            local variable_name = vim.treesitter.query.get_node_text(node, 0)
-
-            if node_type ~= "identifier" then
-                return nil
-            else
-                return variable_name
-            end
-        end
-    end
-end
-
-local get_visual_selection = function()
-    local mode = vim.fn.mode():lower()
-    if not (mode:find("^v") or mode:find("^ctrl-v")) then
-        return nil
-    end
-
-    local first_pos, last_pos = vim.fn.getpos("v"), vim.fn.getpos(".")
-
-    local line1 = first_pos[2] - 1
-    local line2 = last_pos[2] - 1
-    local col1 = first_pos[3] - 1
-    local col2 = last_pos[3]
-
-    if line2 < line1 or (line1 == line2 and col2 < col1) then
-        local linet = line2
-        line2 = line1
-        line1 = linet
-
-        local colt = col2
-        col2 = col1
-        col1 = colt
-
-        col1 = col1 - 1
-        col2 = col2 + 1
-    end
-
-    if line1 ~= line2 then
-        vim.notify(
-            "debugprint not supported when multiple lines selected.",
-            vim.log.levels.ERROR
-        )
-        return false
-    end
-
-    return vim.api.nvim_buf_get_text(0, line1, col1, line2, col2, {})[1]
-end
-
-local get_operator_selection = function()
-    local first_pos, last_pos = vim.fn.getpos("'["), vim.fn.getpos("']")
-
-    local line1 = first_pos[2] - 1
-    local line2 = last_pos[2] - 1
-    local col1 = first_pos[3] - 1
-    local col2 = last_pos[3]
-
-    if line1 ~= line2 then
-        vim.notify(
-            "debugprint not supported when multiple lines in motion.",
-            vim.log.levels.ERROR
-        )
-        return false
-    end
-
-    return vim.api.nvim_buf_get_text(0, line1, col1, line2, col2, {})[1]
-end
-
 local debugprint_addline = function(funcopts)
     local current_line = vim.api.nvim_win_get_cursor(0)[1]
     local filetype =
@@ -203,7 +119,7 @@ M.debugprint_cache = function(o)
         end
 
         if o.variable == true then
-            o.variable_name = get_visual_selection()
+            o.variable_name = utils.get_visual_selection()
 
             if o.variable_name == false then
                 return
@@ -214,7 +130,7 @@ M.debugprint_cache = function(o)
                 and o.ignore_treesitter ~= true
                 and opts.ignore_treesitter ~= true
             then
-                o.variable_name = find_treesitter_variable()
+                o.variable_name = utils.find_treesitter_variable()
             end
 
             if o.variable_name == nil then
@@ -264,7 +180,7 @@ M.debugprint = function(o)
 end
 
 M.debugprint_motion_callback = function()
-    cache_request.variable_name = get_operator_selection()
+    cache_request.variable_name = utils.get_operator_selection()
     debugprint_addline(cache_request)
     set_callback("v:lua.require'debugprint'.debugprint_cache")
 end
---
+++ lua/debugprint/utils.lua
@@ -0,0 +1,89 @@
+local M = {}
+
+M.find_treesitter_variable = function()
+    local function requiref(module)
+        require(module)
+    end
+
+    local ts_utils_test = pcall(requiref, "nvim-treesitter.ts_utils")
+
+    if not ts_utils_test then
+        return nil
+    else
+        local ts_utils = require("nvim-treesitter.ts_utils")
+        -- Once get_node_at_cursor() is in NeoVim core, the nvim-treesitter
+        -- dependency can be removed: https://github.com/neovim/neovim/pull/18232
+        local node = ts_utils.get_node_at_cursor()
+
+        if node == nil then
+            return nil
+        else
+            local node_type = node:type()
+            local variable_name = vim.treesitter.query.get_node_text(node, 0)
+
+            if node_type ~= "identifier" then
+                return nil
+            else
+                return variable_name
+            end
+        end
+    end
+end
+
+M.get_visual_selection = function()
+    local mode = vim.fn.mode():lower()
+    if not (mode:find("^v") or mode:find("^ctrl-v")) then
+        return nil
+    end
+
+    local first_pos, last_pos = vim.fn.getpos("v"), vim.fn.getpos(".")
+
+    local line1 = first_pos[2] - 1
+    local line2 = last_pos[2] - 1
+    local col1 = first_pos[3] - 1
+    local col2 = last_pos[3]
+
+    if line2 < line1 or (line1 == line2 and col2 < col1) then
+        local linet = line2
+        line2 = line1
+        line1 = linet
+
+        local colt = col2
+        col2 = col1
+        col1 = colt
+
+        col1 = col1 - 1
+        col2 = col2 + 1
+    end
+
+    if line1 ~= line2 then
+        vim.notify(
+            "debugprint not supported when multiple lines selected.",
+            vim.log.levels.ERROR
+        )
+        return false
+    end
+
+    return vim.api.nvim_buf_get_text(0, line1, col1, line2, col2, {})[1]
+end
+
+M.get_operator_selection = function()
+    local first_pos, last_pos = vim.fn.getpos("'["), vim.fn.getpos("']")
+
+    local line1 = first_pos[2] - 1
+    local line2 = last_pos[2] - 1
+    local col1 = first_pos[3] - 1
+    local col2 = last_pos[3]
+
+    if line1 ~= line2 then
+        vim.notify(
+            "debugprint not supported when multiple lines in motion.",
+            vim.log.levels.ERROR
+        )
+        return false
+    end
+
+    return vim.api.nvim_buf_get_text(0, line1, col1, line2, col2, {})[1]
+end
+
+return M
