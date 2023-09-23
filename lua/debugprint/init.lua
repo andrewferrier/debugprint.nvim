92a6dd1a5958a7f951aa70c37d48ebdd20cd8203
Andrew Ferrier <andrewferrier@example.com>
2023-09-23 20:02:01 +0100

    feat: Make keymappings unique

--- lua/debugprint/init.lua
+++ lua/debugprint/init.lua
@@ -333,36 +333,42 @@ M.setup = function(opts)
         end, {
             expr = true,
             desc = "Plain debug below current line",
+            unique = true,
         })
         vim.keymap.set("n", "g?P", function()
             return M.debugprint({ above = true })
         end, {
             expr = true,
             desc = "Plain debug above current line",
+            unique = true,
         })
         vim.keymap.set({ "n", "x" }, "g?v", function()
             return M.debugprint({ variable = true })
         end, {
             expr = true,
             desc = "Variable debug below current line",
+            unique = true,
         })
         vim.keymap.set({ "n", "x" }, "g?V", function()
             return M.debugprint({ above = true, variable = true })
         end, {
             expr = true,
             desc = "Variable debug above current line",
+            unique = true,
         })
         vim.keymap.set("n", "g?o", function()
             return M.debugprint({ motion = true })
         end, {
             expr = true,
             desc = "Text-obj-selected variable debug below current line",
+            unique = true,
         })
         vim.keymap.set("n", "g?O", function()
             return M.debugprint({ motion = true, above = true })
         end, {
             expr = true,
             desc = "Text-obj-selected variable debug above current line",
+            unique = true,
         })
     end
 
