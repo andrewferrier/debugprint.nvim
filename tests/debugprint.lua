071f8ea8b7dfb9fc7375179766ad1c2f9bc1d943
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2023-11-14 10:15:01 +0000

    test: Improve robustness of treesitter test locally

--- tests/debugprint.lua
+++ tests/debugprint.lua
@@ -1,3 +1,12 @@
+vim.o.hidden = true
+vim.o.swapfile = false
+
+vim.cmd('set rtp+=~/.local/share/nvim/site/pack/vendor/start/nvim-treesitter')
+vim.cmd('set rtp+=../nvim-treesitter')
+vim.cmd('runtime! plugin/nvim-treesitter.lua')
+vim.cmd('TSInstallSync! bash')
+vim.cmd('TSInstallSync! lua')
+
 local debugprint = require("debugprint")
 
 local check_lines = function(lines)
--- tests/minimal.vim
+++ tests/minimal.vim
@@ -1,16 +1,4 @@
-set hidden
-set noswapfile
-
 set rtp+=~/.local/share/nvim/site/pack/vendor/start/plenary.nvim
-set rtp+=~/.local/share/nvim/site/pack/vendor/start/nvim-treesitter
-
 set rtp+=../plenary.nvim
-set rtp+=../nvim-treesitter
-
-set rtp+=.
 
 runtime! plugin/plenary.vim
-runtime! plugin/nvim-treesitter.lua
-
-TSInstallSync! bash
-TSInstallSync! lua
