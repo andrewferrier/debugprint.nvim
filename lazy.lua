265b0706e83a60817bbda7475fc549832a9e467a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-10 19:19:24 +0100

    fix: Temporary workaround for lazy-loading issue

--- lazy.lua
+++ lazy.lua
@@ -1,15 +1,16 @@
 local spec = {
     url = "https://github.com/andrewferrier/debugprint.nvim",
     opts = {},
-    keys = {
-        { "g?", mode = "n" },
-        { "g?", mode = "x" },
-    },
-    cmd = {
-        "ToggleCommentDebugPrints",
-        "DeleteDebugPrints",
-    },
-    lazy = true,
+    -- FIXME: Work around temporary issue with lazy-loading
+    -- keys = {
+    --     { "g?", mode = "n" },
+    --     { "g?", mode = "x" },
+    -- },
+    -- cmd = {
+    --     "ToggleCommentDebugPrints",
+    --     "DeleteDebugPrints",
+    -- },
+    -- lazy = true,
     -- FIXME: Only introduce this line when we are about to release the next stable version otherwise this will cause a yo-yo effect
     -- Use stable versions by default
     -- version = '*',
