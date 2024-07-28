2129731b785a730beba293939858d39bdb259c2c
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-07-28 14:11:04 +0100

    chore: Remove comment about stable version
    Decided it's not up to me to make this defaulting choice for the user.

--- lazy.lua
+++ lazy.lua
@@ -1,8 +1,5 @@
 local spec = {
     url = "https://github.com/andrewferrier/debugprint.nvim",
-    -- FIXME: Only introduce this line when we are about to release the next stable version otherwise this will cause a yo-yo effect
-    -- Use stable versions by default
-    -- version = '*',
 }
 
 if vim.fn.has('nvim-0.10') == 0 then
