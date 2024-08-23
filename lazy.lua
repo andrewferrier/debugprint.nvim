b7c5fe6164a81a37bbcd972af7f7f82fa55387ee
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-08-23 21:49:00 +0100

    style: Format

--- lazy.lua
+++ lazy.lua
@@ -2,9 +2,9 @@ local spec = {
     url = "https://github.com/andrewferrier/debugprint.nvim",
 }
 
-if vim.fn.has('nvim-0.10') == 0 then
+if vim.fn.has("nvim-0.10") == 0 then
     spec.dependencies = {
-        "echasnovski/mini.nvim" -- Needed to enable :ToggleCommentDebugPrints for NeoVim 0.9
+        "echasnovski/mini.nvim", -- Needed to enable :ToggleCommentDebugPrints for NeoVim 0.9
     }
 end
 
