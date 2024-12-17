e74e3653fb5893a548bfb4a7dd684a07570d6d62
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2024-12-17 14:05:32 +0000

    refactor: Simplify get_variable_name

--- lua/debugprint/init.lua
+++ lua/debugprint/init.lua
@@ -246,15 +246,10 @@ M.debugprint_regular = function(opts)
         local filetype_config = get_filetype_config()
 
         if filetype_config then
-            local ignore_treesitter
-
-            if global_opts.ignore_treesitter or opts.ignore_treesitter then
-                -- Works around issue with optional types
-                ignore_treesitter = true
-            end
-
-            opts.variable_name =
-                utils.get_variable_name(ignore_treesitter, filetype_config)
+            opts.variable_name = utils.get_variable_name(
+                global_opts.ignore_treesitter or opts.ignore_treesitter or false,
+                filetype_config
+            )
 
             if not opts.variable_name then
                 return
